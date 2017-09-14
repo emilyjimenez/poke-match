2e9488453b2e4d6a614eace5d5389e96980a7544
Emily Wells Jimenez and Rane Fields <rane@>
2017-09-14 15:54:42 -0700

    Add starter evolutions dependant upon age

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,56 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var pokemon = "";
-    var pokemonimg = "";
+    var pokemon;
+    var pokemonimg;
+    var age = $("input#age").val();
     var elements = $("input:radio[name=elements]:checked").val();
     var personality = $("#personality").val();
 
     if (personality === "Lazy") {
       pokemon = "Snorlax";
       pokemonimg ="img/snorlax.gif";
+
+      // Fire
     } else if (elements === "fire") {
-      pokemon = "Charmander";
-      pokemonimg = "img/charmander.gif";
+      if (age >= 30) {
+        pokemon = "Charizard";
+        pokemonimg = "img/charizard.gif";
+      }
+      else {
+        pokemon = "Charmander";
+        pokemonimg = "img/charmander.gif";
+      }
+
+      // Water
     } else if (elements === "water") {
-      pokemon = "Squirtle";
-      pokemonimg = "img/squirtle.gif";
+      if (age >= 30) {
+        pokemon = "Blastoise";
+        pokemonimg = "img/blastoise.gif";
+      } else {
+        pokemon = "Squirtle";
+        pokemonimg = "img/squirtle.gif";
+      }
+
+      // Grass
     } else if (elements === "grass") {
-      pokemon = "Bulbasaur";
-      pokemonimg = "img/bulbasaur.gif";
+      if (age >= 30) {
+        pokemon = "Venusaur";
+        pokemonimg = "img/venusaur.gif";
+      } else {
+        pokemon = "Bulbasaur";
+        pokemonimg = "img/bulbasaur.gif";
+      }
+
+      // Electric
     } else if (elements === "electric") {
-      pokemon = "Pikachu"
-      pokemonimg = "img/pikachu.gif"
+      if (age >= 30) {
+        pokemon = "Raichu";
+        pokemonimg = "img/raichu.gif";
+      } else {
+        pokemon = "Pikachu"
+        pokemonimg = "img/pikachu.gif"
+      }
     }
 
     $("#results").removeClass("hide");
